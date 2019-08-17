69002b3c472200764a0ad71d7bd1384fd0e29251
Todd Davis <tejastcd@gmail.com>
2019-08-17 12:02:06 -0400

    new static client branch test

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -11,7 +11,7 @@ export default class LandingPage extends Component {
 
     handleLoginSuccess = () => {
         const { location, history } = this.props
-        const destination = (location.state || {}).from || '/gallery'
+        const destination = (location.state || {}).from || '/collection'
         history.push(destination)
     }
 
