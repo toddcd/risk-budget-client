a7a6d9a765f3ef19bb94bac79f0a4925cb526848
Todd Davis <tejastcd@gmail.com>
2019-08-19 11:37:18 -0400

    landing layout and styling

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -10,10 +10,16 @@ export default class Landing extends Component {
     render() {
         return (
             <div className='landing-container'>
-                <h1 className='question'>How do you budget for risk?</h1>
-                <section>
-                    <div className='step-section'>
-                        <div className='step-list'>
+                <section className='top-section'>
+                    <div className='top-section-left'>
+                        <span className='question'>What is risk budgeting?</span>
+                    </div>
+                    <div className='top-section-right'>
+                        <span className='answer'>Constructing a portfolio based on the volatility of the underlying funds.</span>
+                    </div>
+                </section>
+                <section className='steps-section'>
+                        <div className='steps-list'>
                             <div className='step'>
                                 <span className='step-num'>1</span> Choose Assets
                             </div>
@@ -24,35 +30,27 @@ export default class Landing extends Component {
                                 <span className='step-num'>3</span> Run Optimization
                             </div>
                         </div>
-                        <div className='step-image'>
+                        <div className='steps-image'>
                             <img alt='bars' className='landing-image' src={require("../../images/adding-machine.jpg")}/>
                         </div>
-                    </div>
                 </section>
-                <section className='landing-section'>
-                    <div className='landing-image'>
-                        {/*<img alt='analog-notes-top' src={require("../../images/landingtop.png")}/>*/}
-                    </div>
-                    <div className='landing-content'>
+                <section className='detail-explanation-section'>
+                    <div>
                         <p>The Risk Budgeting Tool allows an investor to run analysis on a portfolio and use the risk
                             and return of the underlying funds to determine the best asset allocation in order to
                             maximize portfolio performance.</p>
                     </div>
-
                 </section>
-                <section className='landing-section'>
-                    <div className='landing-content'>
+                <section className='bottom-section'>
+                    <div className='bottom-section-left'>
+                        <img alt='analog-notes-bottom' className='landing-image' src={require("../../images/burroughs.jpg")}/>
+                    </div>
+                    <div className='bottom-section-right'>
                         <p>Analyze your portfolio and adjust the asset allocation to meet your current investment
                             strategy.</p>
                         <p>Register to login and get started<br/><br/>
                             Or login with the demo account and take a look!<br/>
                             (username: demo, password: Password1!)</p>
-                    </div>
-                    <div className='landing-image'>
-                        {/*<img alt='analog-notes-bottom' className='landing-image' src={require("../../images/landingbottom.png")}/>*/}
-                    </div>
-                    <div className='landing-image'>
-                        {/*<img alt='bars' className='landing-image' src={require("../../images/bars_v2.png")}/>*/}
                     </div>
                 </section>
             </div>
