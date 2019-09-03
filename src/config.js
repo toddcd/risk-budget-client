ca0d73327830871da3b12b84ce3df39941bf0e12
Todd Davis <tejastcd@gmail.com>
2019-09-03 12:51:01 -0400

    calc engine update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  //API_ENDPOINT: 'http://localhost:8000/api',
   //CALC_ENGINE_ENDPOINT: 'http://localhost:8000/api/riskbudget',
   CALC_ENGINE_ENDPOINT: 'https://mighty-reef-59398.herokuapp.com/api/riskbudget',
-  //API_ENDPOINT: 'https://damp-beach-84980.herokuapp.com/api',
+  API_ENDPOINT: 'https://damp-beach-84980.herokuapp.com/api',
   TOKEN_KEY: 'risk-budget-client-auth-token',
 }
